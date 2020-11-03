77c25e576a9f8724ea3877beead550a3bfe4fab9
ValerioTrinca <valer.tria@gmail.com>
2020-11-03 11:08:28 +0100

    Menu

--- resources/views/partials/header-home.blade.php
+++ resources/views/partials/header-home.blade.php
@@ -14,7 +14,7 @@
                   </li>
               @endif
           @else
-              <li class="dropdown d-flex align-items-center">
+              {{-- <li class="dropdown d-flex align-items-center">
                   <a id="navbarDropdown" class="login dropdown-toggle" href="{{ route('dashboard') }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                       {{ Auth::user()->firstname }}
                   </a>
@@ -30,7 +30,42 @@
                           @csrf
                       </form>
                   </div>
-              </li>
+              </li> --}}
+
+              
+              <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <img src="{{asset('img_db/users')}}/{{Auth::user() -> img}}" class="img-profile-header">
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" id="options">
+
+                    <a href='{{ route('dashboard') }}' class="dropdown-item">
+                        Dashboard
+                    </a>
+
+                    <a href=' {{route('prop.create')}}' class="dropdown-item">
+                        Aggiungi properità
+                    </a>
+
+                    <a href='{{route('upr.update')}}' class="dropdown-item">
+                        Modifica account 
+                    </a>
+
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                
+
+               
+                </div>
+            </li>
           @endguest
       </ul>
     </div>
