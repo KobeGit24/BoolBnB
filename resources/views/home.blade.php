a2a976829f1269ecfa74623c30711ef9834c8bd3
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-11-04 13:45:21 +0100

    Aggiornato frontend home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div id="gap-header"></div>
-    <h1 class="text-primary">Dove ti piacerebbe andare?</h1>
+    <h1 class="homeh1">Dove ti piacerebbe andare?</h1>
     <section id="prop-sponsored" class="">
       <div class="sponsored">
         <div class="title text-center p-2">
           <h3>Appartamenti in evidenza</h3>
         </div>
-        <div class="">
+        <div class="home-sponsored-container">
           <div class="d-flex flex-wrap justify-content-around">
             @foreach ($data_property as $d_p)
               <div class="property-sponsored d-flex flex-column justify-content-between index-sponsored card-shadow">
